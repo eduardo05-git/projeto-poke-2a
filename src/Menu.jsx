15e241f442368a2adfcff324ab2af6e2acf1169e
eduardo05-git <rm93771@estudante.fieb.edu.br>
2025-06-02 14:06:38 +0000

    menu

---
+++ src/Menu.jsx
@@ -0,0 +1,19 @@
+import './Menu.css'
+
+const Menu =()=>{
+
+    return (
+        <div>
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="/home">
+                       
+                        Home
+
+                    </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+export default Menu
