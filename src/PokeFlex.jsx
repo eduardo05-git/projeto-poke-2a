1c6739a579a6e35c75dde75e2619f827249b924d
eduardo05-git <rm93771@estudante.fieb.edu.br>
2025-06-05 12:19:04 +0000

    menu

--- src/PokeFlex.jsx
+++ src/PokeFlex.jsx
@@ -1,20 +1,23 @@
 import poke from './assets/pokeapi_256.png'
 import eevee from './assets/eevee.jpg'
 import './PokeFlex.css'
-const PokeFlex = () =>{
+import Menu from './Menu'
+const PokeFlex = () => {
 
     return (
-        <div className="PokeFlex">
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
+        <div>
+            <div className="PokeFlex">
+                <img src={poke} />
+                <img src={eevee} />
+                <img src={poke} />
+                <img src={eevee} />
+                <img src={poke} />
+                <img src={eevee} />
+                <img src={poke} />
+                <img src={eevee} />
+                <img src={poke} />
+                <img src={eevee} />
+            </div>
         </div>
     )
 }
